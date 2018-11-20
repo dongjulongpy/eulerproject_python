74b51da31458db49a0ca1d8942df4cb503963825
Dong Ju-Long <44946009+dongjulongpy@users.noreply.github.com>
2018-11-20 18:49:25 +0300

    Add files via upload

---
+++ smallest_multiple.py
@@ -0,0 +1,19 @@
+def gcd(a, b):
+
+    min_n = min(a, b)
+    max_n = max(a, b)
+
+    if min_n == 0: return a
+    else: return gcd(min_n, max_n%min_n)
+
+def gcm(a, b):
+    return (a*b)/gcd(a,b)
+
+smallest = 1
+
+for i in range(1, 20):
+    gc = gcm(i, i+1)
+    smallest = gcm(gc, smallest)
+
+print(smallest)
+    
